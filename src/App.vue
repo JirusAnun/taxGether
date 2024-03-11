ac412b3536d1eafecf6fe1a52d5600218ba93e99
Jiruschai Anuntabundit <hamjirus@gmail.com>
2024-03-11 13:27:19 +0700

    nav bar skeleton

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <script setup>
 import banner from './components/banner.vue'
+import navBar from './components/navbar.vue'
 </script>
 
 <template>
+  <navBar/>
   <banner/>
 </template>
--- src/components/navbar.vue
+++ src/components/navbar.vue
@@ -1,3 +1,19 @@
 <script setup>
 
 </script>
+<template>
+    <div class="flex justify-between">
+        <div>Taxgether</div>
+        <div class="flex">
+            <div>Products</div>
+            <div>Developer</div>
+            <div>Use cases</div>
+            <div>Company</div>
+            <div>Pricing</div>
+        </div>
+        <div class="flex">
+            <div>Log in</div>
+            <div>Sign Up</div>
+        </div>
+    </div>
+</template>
